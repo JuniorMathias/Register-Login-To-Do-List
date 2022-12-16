b22edc5ae8085d4f22f0906cf9ae59bf3f5a14eb
junior <juniormathiias@gmail.com>
2022-12-16 14:22:46 -0300

    just finished the styled of admin page adding some buttons, textarea

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,9 +1,43 @@
 import * as S from './styles';
+import { useState } from 'react';
+import { auth } from '../../firebaseConnection';
+import { signOut } from 'firebase/auth'; 
 
 export default function Admin(){
+  const [taskInput, setTaskInput] = useState('');
+
+
+  function handleRegister(e){
+      e.preventDefault();
+      alert("click");
+  }
+  async  function handleLoagout(e){
+    await signOut(auth);
+}
+
     return(
-        <S.Div>
-            <h1>admin</h1>
-        </S.Div>
+        <S.Container>
+        <S.Header>My Tasks</S.Header>
+
+        <S.Form onSubmit={handleRegister}>
+          <S.Textarea 
+            placeholder="Type your task..."
+            value={taskInput}
+            onChange={(e) => setTaskInput(e.target.value)}
+          />
+          <S.Button type="submit">Register Tasks</S.Button>
+        </S.Form>
+        <S.Article>
+            <S.Paragraph>
+                Study JavaScript and ReactJs tonight
+            </S.Paragraph>
+            <S.Buttons>
+                <S.ButtonEdit>Edit</S.ButtonEdit>
+                <S.ButtonDone>Done</S.ButtonDone>
+            </S.Buttons>
+        </S.Article>
+        <S.ButtonLogout onClick={handleLoagout}>Log out</S.ButtonLogout>
+
+        </S.Container>
     )
 }
--- src/pages/Admin/styles.js
+++ src/pages/Admin/styles.js
@@ -1,5 +1,94 @@
 import styled from 'styled-components';
 
-export const Div = styled.div`
-  color: #f3a222;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 48px;
+`;
+
+export const Header = styled.h1`
+  margin-bottom: 28px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  max-width: 600px;
+`;
+export const Article = styled.article`
+  width: 90%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(18,18,18, 0.38);
+  border-radius: 4px;
+  margin-bottom: 12px;
+  justify-content: center;
+  padding: 14px 8px;
+`;
+export const Paragraph = styled.p`
+  margin-bottom: 8px;
+`;
+
+export const Textarea = styled.textarea`
+  margin-bottom: 12px;
+  border: 0;
+  height: 90px;
+  resize: none;
+  border-radius: 4;
+  padding 8px;
+`;
+// ------- Buttons Area
+export const Button = styled.button`
+  height: 36px;
+  border: 0;
+  border-radius: 4px;
+  background-color: #3366ff;
+  color: #fff;
+  font-size: 18px;
+  margin-bottom: 28px;
+`;
+
+export const Buttons = styled.div`
+  margin-right: 8px;
+  border: 0;
+  border-radius: 4px;
+  padding: 4px;
+`;
+export const ButtonEdit = styled.button`
+  margin-right: 8px;
+  border: 0;
+  border-radius: 4px;
+  padding: 4px;
+  font-size: 15px;
+`;
+
+export const ButtonDone = styled.button`
+  color: #ffcc23;
+  background-color: transparent;
+  margin-right: 8px;
+  border: 0;
+  border-radius: 4px;
+  padding: 4px;
+  font-size: 15px;
+`;
+
+export const ButtonLogout = styled.button`
+   position: absolute;
+   bottom: 6%;
+   left: 4%;
+   height: 60px;
+   width: 60px;
+   border-radius: 30px;
+   border: 0;
+   font-weight: bold;
+   background-color: rgba(219, 38, 41, 0.35);
+   color: #fafafa;
+   transition: all 0.5s;
+   &:hover {
+    background-color: rgba(219, 38, 41, 1);
+   color: #fff;
+  }
 `;
